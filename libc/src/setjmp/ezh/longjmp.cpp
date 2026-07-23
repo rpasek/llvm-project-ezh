llvm/llvm-project

[libc] Add EZH baremetal support
This commit adds EZH architecture support to LLVM's libc, including
baremetal entrypoints, headers, and target-specific setjmp/longjmp C++
implementations.

Information sources:
* SmartDMA Cookbook (AN14650.pdf)
* fsl_smartdma_prv.h

libc/src/setjmp/ezh/longjmp.cpp
@@ -1,0 +1,38 @@
+//===-- Implementation of longjmp for EZH ---------------------------------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
+#include "src/__support/common.h"
+#include "src/__support/macros/config.h"
+#include "src/setjmp/longjmp.h"
+
+namespace LIBC_NAMESPACE_DECL {
+
+[[gnu::naked]] LLVM_LIBC_FUNCTION(void, longjmp, (jmp_buf buf, int val)) {
+  asm(R"(
+      # Restore r4, r5, r6, r7, sp, and ra from buf (r0)
+      e_ldr r4, r0, 0
+      e_ldr r5, r0, 4
+      e_ldr r6, r0, 8
+      e_ldr r7, r0, 12
+      e_ldr sp, r0, 16
+      e_ldr ra, r0, 20
+
+      # Move val (passed in r1) to return register r0
+      e_mov r0, r1
+
+      # If val is 0, longjmp must return 1 instead
+      e_sub_imms r1, r0, 0
+      e_goto_nz .Ljump_back
+      e_load_imm r0, 1
+
+  .Ljump_back:
+      e_goto ra
+  )");
+}
+
+} // namespace LIBC_NAMESPACE_DECL

libc/src/setjmp/ezh/setjmp.cpp
@@ -1,0 +1,31 @@
+//===-- Implementation of setjmp for EZH ----------------------------------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
+#include "src/__support/common.h"
+#include "src/__support/macros/config.h"
+#include "src/setjmp/setjmp_impl.h"
+
+namespace LIBC_NAMESPACE_DECL {
+
+[[gnu::naked]] LLVM_LIBC_FUNCTION(int, setjmp, (jmp_buf buf)) {
+  asm(R"(
+      # Store r4, r5, r6, r7, sp, and ra into buf (passed in r0)
+      e_str r0, r4, 0
+      e_str r0, r5, 4
+      e_str r0, r6, 8
+      e_str r0, r7, 12
+      e_str r0, sp, 16
+      e_str r0, ra, 20
+
+      # Return 0
+      e_load_imm r0, 0
+      e_goto ra
+  )");
+}
+
+} // namespace LIBC_NAMESPACE_DECL

rpasek/llvm-project-ezh

libc/src/setjmp/ezh/longjmp.cpp
@@ -1,0 +1,38 @@
+//===-- Implementation of longjmp for EZH ---------------------------------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
+#include "src/setjmp/longjmp.h"
+#include "src/__support/common.h"
+#include "src/__support/macros/config.h"
+
+namespace LIBC_NAMESPACE_DECL {
+
+[[gnu::naked]] LLVM_LIBC_FUNCTION(void, longjmp, (jmp_buf buf, int val)) {
+  asm(R"(
+      # Restore r4, r5, r6, r7, sp, and ra from buf (r0)
+      ldr r4, r0, 0
+      ldr r5, r0, 4
+      ldr r6, r0, 8
+      ldr r7, r0, 12
+      ldr sp, r0, 16
+      ldr ra, r0, 20
+
+      # Move val (passed in r1) to return register r0
+      mov r0, r1
+
+      # If val is 0, longjmp must return 1 instead
+      sub_imms r1, r0, 0
+      goto_nz .Ljump_back
+      load_imm r0, 1
+
+  .Ljump_back:
+      goto_reg ra
+  )");
+}
+
+} // namespace LIBC_NAMESPACE_DECL

libc/src/setjmp/ezh/setjmp.cpp
@@ -1,0 +1,31 @@
+//===-- Implementation of setjmp for EZH ----------------------------------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
+#include "src/__support/common.h"
+#include "src/__support/macros/config.h"
+#include "src/setjmp/setjmp_impl.h"
+
+namespace LIBC_NAMESPACE_DECL {
+
+[[gnu::naked]] LLVM_LIBC_FUNCTION(int, setjmp, (jmp_buf buf)) {
+  asm(R"(
+      # Store r4, r5, r6, r7, sp, and ra into buf (passed in r0)
+      str r0, r4, 0
+      str r0, r5, 4
+      str r0, r6, 8
+      str r0, r7, 12
+      str r0, sp, 16
+      str r0, ra, 20
+
+      # Return 0
+      load_imm r0, 0
+      goto_reg ra
+  )");
+}
+
+} // namespace LIBC_NAMESPACE_DECL